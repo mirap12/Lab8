86289675b75be43754f99121941ac24020aa6d89
Mira <mirapatel2512@gmail.com>
2022-10-26 17:33:30 -0600

    fails

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -59,7 +59,8 @@ public class CustomList extends ArrayAdapter<City> {
      * @param city
      */
     public void addCity(City city){
-        cities.add(city);
+
+        //cities.add(city);
     }
 
 
