dcd652c214cf4c479d3ea8a2ad9ecad393650d7d
Mira <mirapatel2512@gmail.com>
2022-10-26 17:25:12 -0600

    new add city test written

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,31 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){   //there is a custom list containing an empty arry list. THis will be used for the test.
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void addCityTest(){
+        // this is a test which will get the size of the list to us, then increase the list by adding a new city.
+        //   - Check if: the current size matches the initial size plus one.
+        list = MockCityList();
+        int listSize = list.getCount();                          //get initial list size
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);        //Check if: the current size matches the initial size plus one.
+    }
+
 
 }
